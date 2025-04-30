9d7d55a991d329d1dbfe53d7589510d7ef5efd2f
Aaron1002 <h44114025@gs.ncku.edu.tw>
2025-04-30 09:06:24 +0000

    Exercise2_1_completed_v1.0

--- bit_reverse.c
+++ bit_reverse.c
@@ -1,30 +1,30 @@
     "addi %[add_cnt], %[add_cnt], 3\n\t"
-"addi t0, x0, 1\n\t" // fix t0 to 1 (for bne)
-"add t1, x0, x0\n\t" // initiallize t1 (temp_ans)
-"add t2, x0, x0\n\t" // initiallize t2 (temp b)
+"addi t0, x0, 1\n\t" 
+"add t1, x0, x0\n\t" 
+"add t2, x0, x0\n\t" 
 
 "Loop_bit:\n\t"
         "addi %[others_cnt], %[others_cnt], 1\n\t"
-    "beq %[m], x0, Exit_bit\n\t" // If m == iter, exit loop
+    "beq %[m], x0, Exit_bit\n\t" 
     
         "addi %[others_cnt], %[others_cnt], 3\n\t"
-    "slli t1, t1, 1\n\t" // ans << 1 (shift left 1 bit)
+    "slli t1, t1, 1\n\t" 
 
-    "andi t2, %[b], 1\n\t" // t2 = b & 1 (get the last bit of b)
-    "bne t2, t0, Update_b\n\t" // If t2 != 1, go to update_b 
+    "andi t2, %[b], 1\n\t" 
+    "bne t2, t0, Update_b\n\t" 
 
         "addi %[others_cnt], %[others_cnt], 2\n\t"
-    "ori t1, t1, 1\n\t" // ans = ans | 1 (ans + 1)
+    "ori t1, t1, 1\n\t" 
 
-    "jal x0, Update_b\n\t" // Jump to update_b
+    "jal x0, Update_b\n\t" 
 
 "Update_b:\n\t"
         "addi %[add_cnt], %[add_cnt], 1\n\t"
         "addi %[others_cnt], %[others_cnt], 2\n\t"
-    "srli %[b], %[b], 1\n\t" // b >> 1 (shift right 1 bit)
-    "addi %[m], %[m], -1\n\t" // Increase counter by 1
-    "jal x0, Loop_bit\n\t" // Jump to the beginning of the loop
+    "srli %[b], %[b], 1\n\t" 
+    "addi %[m], %[m], -1\n\t" 
+    "jal x0, Loop_bit\n\t" 
 
 "Exit_bit:\n\t"
         "addi %[add_cnt], %[add_cnt], 1\n\t"
-    "add %[b], t1, x0\n\t" // b = ans    
+    "add %[b], t1, x0\n\t"  
--- pi.c
+++ pi.c
@@ -1,44 +1,44 @@
     "addi %[add_cnt], %[add_cnt], 4\n\t"
-"addi t1, x0, 0\n\t" // initiallize t1 (fix to 0 for iteration)
-"addi t2, x0, 1\n\t" // initiallize t2 (after inintialization, t2 is for counter % 2)
-"addi t3, x0, 1\n\t" // initiallize t3 (after inintialization, t3 is for rest f1)
-"addi t4, x0, 2\n\t" // initiallize t4 (fix to 2 for denominator) 
+"addi t1, x0, 0\n\t" 
+"addi t2, x0, 1\n\t" 
+"addi t3, x0, 1\n\t" 
+"addi t4, x0, 2\n\t" 
 
     "addi %[others_cnt], %[others_cnt], 2\n\t"
-"fcvt.s.wu f1, t2\n\t" // convert t2 to float (f1 initial to 1.0)
-"fcvt.s.wu f2, t3\n\t" // convert t3 to float (f2 initial to 1.0)
+"fcvt.s.wu f1, t2\n\t" 
+"fcvt.s.wu f2, t3\n\t" 
 
 "Loop_pi:\n\t"
         "addi %[others_cnt], %[others_cnt], 1\n\t"
-    "beq t1, %[N], Exit_pi\n\t" // If counter(t1) == iter, exit loop
+    "beq t1, %[N], Exit_pi\n\t" 
         "addi %[others_cnt], %[others_cnt], 3\n\t"
         "addi %[add_cnt], %[add_cnt], 1\n\t"
         "addi %[fdiv_cnt], %[fdiv_cnt], 1\n\t"
-    "fcvt.s.wu f1, t3\n\t" // reset f1 to 1.0
-    "addi t1, t1, 1\n\t" // Increase counter by 1
+    "fcvt.s.wu f1, t3\n\t" 
+    "addi t1, t1, 1\n\t" 
     
-    "fdiv.s f1, f1, f2\n\t" // f1 = f1 / f2
+    "fdiv.s f1, f1, f2\n\t" 
     
-    "rem t2, t1, t4\n\t" // t2 = counter % 2
-    "beq t2, x0, Even_pi\n\t" // If counter % 2 == 0, go to Even_pi (counter is even)
+    "rem t2, t1, t4\n\t" 
+    "beq t2, x0, Even_pi\n\t" 
         "addi %[others_cnt], %[others_cnt], 1\n\t"
-    "jal x0, Odd_pi\n\t" // Else, go to Odd_pi (counter is odd)
+    "jal x0, Odd_pi\n\t" 
 
 "Even_pi:\n\t"
         "addi %[fsub_cnt], %[fsub_cnt], 1\n\t"
         "addi %[fadd_cnt], %[fadd_cnt], 1\n\t"
         "addi %[others_cnt], %[others_cnt], 2\n\t"
-    "fsub.s %[pi], %[pi], f1\n\t" // pi = pi - f1
-    "fcvt.s.wu f1, t4\n\t" // convert t4 to float (for update f2) 
-    "fadd.s f2, f2, f1\n\t" // f2 = f2 + 2.0 (f2 is updated to next denominator)
-    "jal x0, Loop_pi\n\t" // Jump to the beginning of the loop
+    "fsub.s %[pi], %[pi], f1\n\t" 
+    "fcvt.s.wu f1, t4\n\t" 
+    "fadd.s f2, f2, f1\n\t" 
+    "jal x0, Loop_pi\n\t" 
 
 "Odd_pi:\n\t"
         "addi %[fadd_cnt], %[fadd_cnt], 2\n\t"
         "addi %[others_cnt], %[others_cnt], 2\n\t"
-    "fadd.s %[pi], %[pi], f1\n\t" // pi = pi + f1
-    "fcvt.s.wu f1, t4\n\t" // convert t4 to float (for update f2) 
-    "fadd.s f2, f2, f1\n\t" // f2 = f2 + 2.0 (f2 is updated to next denominator)
-    "jal x0, Loop_pi\n\t" // Jump to the beginning of the loop
+    "fadd.s %[pi], %[pi], f1\n\t" 
+    "fcvt.s.wu f1, t4\n\t" 
+    "fadd.s f2, f2, f1\n\t" 
+    "jal x0, Loop_pi\n\t" 
 
 "Exit_pi:\n\t"
